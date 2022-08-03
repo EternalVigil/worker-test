3453bbed284c3cfa8996625b826d3a9e572c6ce8
EternalVigil <theeternalvigil@gmail.com>
2022-08-03 08:50:25 -0700

    initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
+export const Counter = () => {
+
+    const [normalCounter, setNormalCounter] = useState(0);
+    const [delayCounter, setDelayCounter] = useState(0);
+
+    const delayWorker = useMemo(() => new Worker(new URL('./workers/delayCounter.js', import.meta.url)), []);
+
+    const handleNormalCount = () => {
+        setNormalCounter(normalCounter+1);
+    };
+
+    const handleDelayCount = () => {
+        delayWorker.postMessage({ msg: 'updateDelayCount', currentCount: delayCounter });
+    };
+
+    useEffect(() => {
+        delayWorker.onmessage = (event) => {
+            if(event?.data) {
+                setDelayCounter(event.data);
+            }
+        };
+
+    }, [delayWorker]);
+
+    return(
+        <div className="counterContainer">
+            COUNTER GOES HERE
+            <div>
+                <button onClick={handleNormalCount}>Update Normal Count</button>
+            </div>
+            <div>
+                {normalCounter}
+            </div>
+            <div>
+                <button onClick={handleDelayCount}>
+                    Update Delayed Counter
+                </button>
+            </div>
+            <div>
+                {delayCounter}
+            </div>
+        </div>
+    );
+};
---
+++ src/workers/delayCounter.js
@@ -0,0 +1,13 @@
+const updateDelayCounter = (currentValue) => {
+    const now = Date.now();
+
+    while(Date.now() < now + 5000) {}
+    return currentValue + 1;
+};
+
+onmessage = async (event) => {
+    if(event?.data?.msg === 'updateDelayCount') {
+        const newCount = updateDelayCounter(event.data.currentCount);
+        postMessage(newCount);
+    }
+}
